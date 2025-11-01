bb55e974faf9d0d59ab0f3bc3df9bcdb9b46bb08
2ro <kmlee8403@gmail.com>
2025-11-02 01:50:02 +0900

    Change motor.run to motor.start for movement

---
+++ Measure_Distance.py
@@ -0,0 +1,17 @@
+import time
+from buildhat import DistanceSensor, Motor
+
+motor = Motor('A')
+dist = DistanceSensor('B')
+
+while True:
+    distance = dist.get_distance()
+    
+    if distance < 10:
+        motor.stop()
+        print("물체가 가까워서 멈춤!")
+    else:
+        motor.start(50)
+        print("안전, 이동 중...")
+    
+    time.sleep(0.2)  # 0.2초마다 센서값 확인
